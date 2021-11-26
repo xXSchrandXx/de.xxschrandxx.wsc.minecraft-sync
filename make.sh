30d4b5862563ec696a99167691000d0685f61fdf
xXSchrandXx <marvinschrand@yahoo.de>
2021-11-26 08:14:02 +0100

    Start Minecraft-Sync

---
+++ make.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+PACKAGE_NAME=de.xxschrandxx.wsc.minecraft-sync
+PACKAGE_TYPES=(acptemplates files templates)
+
+for i in "${PACKAGE_TYPES[@]}"
+do
+    rm -rf ${i}.tar
+    7z a -ttar -mx=9 ${i}.tar ./${i}/*
+done
+
+rm -rf ${PACKAGE_NAME}.tar ${PACKAGE_NAME}.tar.gz
+7z a -ttar -mx=9 ${PACKAGE_NAME}.tar ./* -x!acptemplates -x!files -x!templates -x!${PACKAGE_NAME}.tar -x!${PACKAGE_NAME}.tar.gz -x!.git -x!.gitignore -x!make.sh -x!make.bat -x!.github -x!php_cs.dist -x!phpcs.xml -x!Readme.md -x!pictures
+7z a ${PACKAGE_NAME}.tar.gz ${PACKAGE_NAME}.tar
+
+for i in "${PACKAGE_TYPES[@]}"
+do
+    rm -rf ${i}.tar
+done
