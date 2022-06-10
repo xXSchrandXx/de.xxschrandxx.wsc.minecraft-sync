cc406f3a5c9faae269f61558a34b6968931d70a5
xXSchrandXx <marvinschrand@yahoo.de>
2022-06-10 15:28:38 +0200

    Update MinecraftSyncSyncGroupBackgroundJob.class.php

--- files/lib/system/background/job/MinecraftSyncSyncGroupBackgroundJob.class.php
+++ files/lib/system/background/job/MinecraftSyncSyncGroupBackgroundJob.class.php
@@ -3,18 +3,34 @@
 namespace wcf\system\background\job;
 
 use wcf\system\background\job\AbstractBackgroundJob;
+use wcf\system\database\exception\DatabaseQueryException;
+use wcf\system\database\exception\DatabaseQueryExecutionException;
 use wcf\system\minecraft\MinecraftSyncHandler;
 use wcf\system\WCF;
 
 class MinecraftSyncSyncGroupBackgroundJob extends AbstractBackgroundJob
 {
-    protected int $groupID;
+    protected array $userIDChunks = [];
     protected array $removeGroups;
 
     public function __construct(int $groupID, array $removeGroups = [])
     {
-        $this->groupID = $groupID;
         $this->removeGroups = $removeGroups;
+
+        $sql = "SELECT userID
+                FROM wcf".WCF_N."_user_to_group
+                WHERE groupID = ?";
+        try {
+            /** @var \wcf\system\database\statement\PreparedStatement */
+            $statement = WCF::getDB()->prepareStatement($sql);
+            $statement->execute([$this->groupID]);
+            $userIDs = $statement->fetchAll(\PDO::FETCH_COLUMN);
+            $this->userIDChunks = array_chunk($userIDs, MINECRAFT_SYNC_ENTRIES_PER_CALL, true);
+        } catch (DatabaseQueryException | DatabaseQueryExecutionException $e) {
+            if (ENABLE_DEBUG_MODE) {
+                \wcf\functions\exception\logThrowable($e);
+            }
+        }
     }
 
     protected $retryAfter = 3 * 10;
@@ -32,24 +48,28 @@ class MinecraftSyncSyncGroupBackgroundJob extends AbstractBackgroundJob
      */
     public function perform()
     {
-        if (MINECRAFT_SYNC_ENABLED) {
+        if (!MINECRAFT_SYNC_ENABLED) {
             return;
         }
 
-        $sql = "SELECT userID
-                FROM wcf".WCF_N."_user_to_group
-                WHERE groupID = ?";
-        $statement = WCF::getDB()->prepareStatement($sql);
-        $statement->execute([$this->groupID]);
-        $userIDs = $statement->fetchArray();
-
-        $responses = MinecraftSyncHandler::getInstance()->syncMultiple($userIDs, $this->removeGroups);
+        $responses = [];
+        foreach ($this->userIDChunks as $chunkID => $userIDs) {
+            if (!empty($userIDs)) {
+                $responses =+ MinecraftSyncHandler::getInstance()->syncMultipleUser($userIDs, $this->removeGroups);
+            }
+        }
         // TODO fail on TooManyConnections in responses
+        $fail = false;
         foreach ($responses as $minecraftID => $response) {
             if (array_key_exists('retryAfter', $response)) {
                 $this->retryAfter = $response['retryAfter'];
-                $this->fail();
+                $fail = true;
+            } else {
+                
             }
+        }
+        if ($fail) {
+            $this->fail();
         }
     }
 }
