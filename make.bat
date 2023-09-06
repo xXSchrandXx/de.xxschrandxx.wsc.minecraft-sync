e9d69f0b09fe8b9d5c2d54b8f2b099ebc80075e1
xXSchrandXx <marvinschrand@yahoo.de>
2023-09-06 21:23:44 +0200

    Update make

--- make.bat
+++ make.bat
@@ -8,7 +8,7 @@ for %%i in %PACKAGE_TYPES% do (
 
 del %PACKAGE_NAME%.tar
 del %PACKAGE_NAME%.tar.gz
-7z a -ttar -mx=9 %PACKAGE_NAME%.tar .\* -x!acptemplates -x!files -x!templates -x!%PACKAGE_NAME%.tar -x!%PACKAGE_NAME%.tar.gz -x!.git -x!.gitignore -x!.gitattributes -x!make.sh -x!make.bat -x!.github -x!php_cs.dist -x!phpcs.xml -x!Readme.md -x!pictures -x!node_modules -x!package-lock.json -x!package.json -x!tsconfig.json -x!ts -x!constants.php
+7z a -ttar -mx=9 %PACKAGE_NAME%.tar .\* -x!acptemplates -x!files -x!templates -x!%PACKAGE_NAME%.tar -x!%PACKAGE_NAME%.tar.gz -x!.git -x!.gitignore -x!.gitattributes -x!make.sh -x!make.bat -x!.github -x!php_cs.dist -x!.phpcs.xml -x!Readme.md -x!pictures -x!node_modules -x!package-lock.json -x!package.json -x!tsconfig.json -x!ts -x!constants.php
 7z a %PACKAGE_NAME%.tar.gz %PACKAGE_NAME%.tar
 del ".\%PACKAGE_NAME%.tar"
 
